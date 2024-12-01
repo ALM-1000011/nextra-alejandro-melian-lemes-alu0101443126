f88784cc2d88bb97fa536c12f29f3af93a93021b
Alejandro Melián <72447092+ALM-1000011@users.noreply.github.com>
2024-12-01 12:20:43 +0000

    Implementado jsx que realiza fetch a la api de github

---
+++ pages/user/[username].jsx
@@ -0,0 +1,56 @@
+import Link from 'next/link'
+
+export const getServerSideProps = async (context) => {
+  const username = context.params?.username;
+
+  try {
+    const response = await fetch(`https://api.github.com/users/${username}/repos`);
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch repos');
+    }
+    
+    // Asegúrate de que la respuesta no esté vacía
+    const repos = await response.json();
+    
+
+    return {
+      props: {
+        repos,
+        username,
+      },
+    };
+  } catch (error) {
+    console.error('Error in getServerSideProps:', error);
+    return {
+      props: {
+        repos: [],
+        username,
+      },
+    };
+  }
+}
+
+export default function UserRepos({ repos, username }) {
+  return (
+    <div className="mt-4">
+      <h2 className="text-xl font-semibold mb-2">{username}'s Repositories</h2>
+      {repos.length > 0 ? (
+        <ul className="list-disc pl-5">
+          {repos.map((repo) => (
+            <li key={repo.id} className="mb-1">
+              <a href={repo.html_url} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">
+                {repo.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No repositories found.</p>
+      )}
+      <Link href="/" className="mt-4 inline-block text-blue-500 hover:underline bg-blue-highlight">
+        Volver a la página de inicio
+      </Link>
+    </div>
+  );
+}
