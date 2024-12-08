db80a8b8cb78075ebcde8910421f11b9daa0d767
Alejandro Melián <72447092+ALM-1000011@users.noreply.github.com>
2024-12-08 18:06:14 +0000

    Implementando auth

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,13 @@
-// pages/_app.js
-import '../styles/Bleed.module.css'
+import { SessionProvider } from "next-auth/react"
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
-
-export default MyApp
 
